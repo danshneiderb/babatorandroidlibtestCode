194952f94f0bf998f441f6e96f26a5ee2cddbf4f
Dan Shneider <dan.shneider@babator.com>
2016-05-04 12:32:32 +0300

    Initial commit

---
+++ babatorandroidlibtest/src/main/java/babator/com/babatorandroidlibtest/Calc.java
@@ -0,0 +1,16 @@
+package babator.com.babatorandroidlibtest;
+
+/**
+ * Created by Dan on 04/05/2016.
+ */
+public class Calc {
+    /**
+     * Addition of x+y
+     * @param x
+     * @param y
+     * @return
+     */
+    public static int Add(int x, int y){
+        return x + y;
+    }
+}
