3ae822a322393e8090c6036edced693bfcda0d9c
danshneider <giHonda2016>
2016-05-08 10:53:26 +0300

    I

---
+++ calculatormodule/src/main/java/babator/com/calculatormodule/DoCalc.java
@@ -0,0 +1,62 @@
+package babator.com.calculatormodule;
+
+/**
+ * Created by danshneider on 08/05/2016.
+ */
+public class DoCalc {
+    /**
+     * Addition of x+y
+     * @param x
+     * @param y
+     * @return
+     */
+    public static int Add(int x, int y){
+        return x + y;
+    }
+
+    /**
+     * Substruct x-y
+     * @param x
+     * @param y
+     * @return
+     */
+    public static int Subs(int x, int y){
+        return x - y;
+    }
+
+    /**
+     * Multiply x*y
+     * @param x
+     * @param y
+     * @return
+     */
+    public static int Multi(int x, int y){
+        return x * y;
+    }
+
+    /**
+     * Div x/y
+     * @param x
+     * @param y
+     * @return
+     */
+    public static int Div(int x, int y){
+        if(y != 0){
+            return x / y;
+        }
+        else{
+            return 0;
+        }
+
+    }
+
+    /**
+     * X power 2
+     * @param x
+     * @return
+     */
+    public static int Power2(int x){
+        return x^2;
+
+    }
+}
